3b568376d82673db73d5deb30d43ab61df61ce58
Haekyu Park <hkpark627@gmail.com>
2019-10-05 15:44:01 -0400

    left panel has list

--- src/mlsploit/leftpanel/LeftPanel.svelte
+++ src/mlsploit/leftpanel/LeftPanel.svelte
@@ -4,17 +4,32 @@
   }
 </style>
 
-<div id="left-panel">
-Left Panel
-</div>
+
+<!-- TODO: Delete this later -->
+<div id="left-panel">Left Panel</div>
+
+<!-- Create the List for modules -->
+<ul>
+	{#each modules as module}
+		<li>
+      {module.module}
+      <ul>
+        {#each module.function as f}
+          <li>{f}</li>
+        {/each}
+      </ul>
+    </li>
+	{/each}
+</ul>
 
 <script>
   // TODO: Need to get the real modules
-  let modules = {
-    'Mod1': ['Fun1-1', 'Fun1-2', 'Fun1-3'],
-    'Mod2': ['Fun2-1', 'Fun2-2', 'Fun2-3', 'Fun2-4'],
-    'Mod3': ['Fun3-1', 'Fun3-2']
-  }
+  let modules = [
+    {module: 'Mod1', function: ['Fun1-1', 'Fun1-2', 'Fun1-3']},
+    {module: 'Mod2', function: ['Fun2-1', 'Fun2-2', 'Fun2-3', 'Fun2-4']},
+    {module: 'Mod3', function: ['Fun3-1', 'Fun3-2']}
+  ]
+
 
   console.log(modules)
 </script>

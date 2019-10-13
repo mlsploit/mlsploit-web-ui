c5bf037e016fd866e183f11c9818e30eb4b05165
Nilaksh Das <nilakshdas@gatech.edu>
2019-10-13 11:24:26 -0700

    Update header rendering

--- src/mlsploit/header/Header.svelte
+++ src/mlsploit/header/Header.svelte
@@ -9,38 +9,65 @@
 
 <style>
   .logo {
-    height: 40px;
+    display: inline-block;
+    vertical-align: text-bottom;
+  }
+
+  .logo > img {
+    height: 4rem;
+  }
+
+  .brand {
+    margin-left: 0.5rem;
+    display: inline-block;
+  }
+
+  .brand-name {
+    font-size: 2rem;
+    color: white;
   }
   
   .tagline {
-    color: #6c757d;
+    font-size: 1rem;
+    font-weight: 200;
+    color: white;
+  }
+
+  .navbar-buttons {
+    margin-top: 1rem;
   }
 </style>
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
   <div class="container-fluid">
     <div class="row w-100">
-      <div class="col-md-4">
-        <img class="logo" src="/assets/img/mlsploit-icon.png" alt="MLsploit" />
-        <a class="navbar-brand" href="/">MLsploit</a>
-        <span class="tagline">Experiment with AI Security in your browser!</span>
+      <div class="col-md-6">
+            <div class="logo">
+                <img src="/assets/img/mlsploit-icon.png" alt="MLsploit" />
+            </div>
+
+            <div class="brand">
+              <span class="brand-name" href="/"><strong>ML</strong>sploit</span>
+              <br />
+              <span class="tagline">Experiment with AI Security in your browser!</span>
+            </div>
       </div>
       
-      <div class="col-md-4 text-center">
-        <button type="button" class="btn btn-outline-secondary" 
-                on:click={onNewPipelineBtnClicked}>
-          <i class="fas fa-plus-square"></i> New Pipeline
-        </button>
-        
-        <button type="button" class="btn btn-outline-secondary" id="btn-files">
-          <i class="fas fa-file"></i> Files
-        </button>
-      </div>
-      
-      <div class="col-md-4 text-right">
-        <button type="button" class="btn btn-outline-secondary">
-          <i class="fas fa-cog"></i>
-        </button>
+      <div class="col-md-6 text-right">
+        <div class="navbar-buttons">
+          <button type="button" class="btn btn-primary" 
+                  on:click={onNewPipelineBtnClicked}>
+            <i class="fas fa-plus-square"></i> New Pipeline
+          </button>
+          
+          <button type="button" class="btn btn-secondary">
+            <i class="fas fa-file"></i> Files
+          </button>
+          
+          <button type="button" class="btn btn-secondary">
+            <i class="fas fa-cog"></i>
+          </button>
+        </div>
       </div>
     </div>
   </div>

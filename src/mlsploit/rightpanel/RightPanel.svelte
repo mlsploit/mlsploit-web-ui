6ae979c424a8284ce1dbe5e3b1b3a2b410e07b99
Nilaksh Das <nilakshdas@gatech.edu>
2019-10-14 11:25:40 -0700

    Create fixed right panel

--- src/mlsploit/rightpanel/RightPanel.svelte
+++ src/mlsploit/rightpanel/RightPanel.svelte
@@ -1,22 +1,43 @@
 <script>
-  import Module from './modules/module.svelte';
-  import Wears_Glasses from './modules/wears_glasses.svelte'
-  import Age from './modules/age.svelte'
-  import Gender from './modules/gender.svelte'
+  import { onMount } from 'svelte';
+  
+  let detailViewContainer;
+
+  const resizeDetailViewContainer = () => {
+    let windowWidth = jQuery(window).width();
+    let navHeight = jQuery('nav').outerHeight();
+    let rightPanelWidth = jQuery('#right-panel').width();
+
+    if (windowWidth >= 768) {
+      let el = jQuery(detailViewContainer);
+      el.css({'top': navHeight+'px'});
+      el.width(rightPanelWidth);
+    }
+  }
+
+  onMount(resizeDetailViewContainer);
+  jQuery(window).resize(resizeDetailViewContainer);
 </script>
 
 <style>
-  #right-panel {
-    padding: 20px;
-    overflow-y: scroll;
-    height: 100vh;
+  @media (min-width: 768px) {
+    .detail-view-container {
+      position: fixed;
+      bottom: 0;
+      right: 15px;
+      z-index: 1020;
+      border: 2px solid red;
+    }
+  }
+
+  .top-spacer {
+    height: 2rem;
   }
 </style>
 
-<div id="right-panel" class="col-md-3 collapse">
-  <h1> Parameters </h1>
-  <Module />
-  <Age />
-  <Gender />
-  <Wears_Glasses />
+<div id="right-panel" class="col-md-3">
+  <div class="detail-view-container" bind:this={detailViewContainer}>
+    <div class="top-spacer"></div>
+    <span>Right Panel</span>
+  </div>
 </div>

c3dab30601834dc749f1237065123264b3d136f9
Nilaksh Das <nilakshdas@gatech.edu>
2019-10-15 09:35:55 -0700

    Remove extra text box from module detail view

--- src/mlsploit/rightpanel/detailviews/module/ModuleDetailView.svelte
+++ src/mlsploit/rightpanel/detailviews/module/ModuleDetailView.svelte
@@ -8,7 +8,6 @@
   <span>
     {module.name} module description will be shown here.
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    <input type="text" />
   </span>
 
   <div slot="detail-view-footer" class="detail-view-footer">

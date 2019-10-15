410b79eb3d8967d0354a6c4a55f930a514fc8c78
Jay Wong <xiao.hk1997@gmail.com>
2019-10-15 12:44:37 -0400

    Add hover to show larger image

--- src/mlsploit/mainpanel/tasklist/ImageVis.svelte
+++ src/mlsploit/mainpanel/tasklist/ImageVis.svelte
@@ -1,9 +1,23 @@
 <script>
   import data from '../../../dummydata.js';
+  import { onMount } from 'svelte';
   
   export let imageURL;
   export let isInput = false;
 
+  // Enable tooltips
+  onMount(() => {
+    jQuery('[data-toggle="popover"]').popover();
+  });
+
+  // Need to set the width to 100% because the popover max-width is set to
+  // 300px in the global CSS -> scale the original image to 300px when it is
+  // too large
+  let tooltipImageTemplate = `
+    <img src="${imageURL}"
+      alt="Larger Visualization of the input/output image."
+      style="width:100%">
+    ` 
 </script>
 
 <style>
@@ -21,6 +35,13 @@
 
 </style>
 
-<div class="image-vis" class:input={isInput}>
-  <img src={imageURL} alt="Visualization of the input/output image.">
+<div class="image-vis"
+  lass:input={isInput}
+  data-toggle="popover"
+  data-container="main"
+  data-trigger="hover"
+  data-offset=100%p
+  data-html="true"
+  data-content={tooltipImageTemplate}>
+  <img src="{imageURL}" alt="Visualization of the input/output image.">
 </div>

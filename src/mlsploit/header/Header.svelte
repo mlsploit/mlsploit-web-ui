740cb28d4471763e74f8daa13b3c613b264d10fb
Haekyu Park <hkpark627@gmail.com>
2019-10-15 12:58:33 -0400

    commit before merge

--- src/mlsploit/header/Header.svelte
+++ src/mlsploit/header/Header.svelte
@@ -1,13 +1,12 @@
 <script>
-  import { newPipelineVisibleStore } from '../../store.js';
-  import Modal from '../mainpanel/filemanager/Modal.svelte';
+  import { newPipelineVisibleStore, FileManagerVisibleStore } from '../../store.js';
 
   const onNewPipelineBtnClicked = (e) => {
     if (e.preventDefault) { e.preventDefault(); }
     $newPipelineVisibleStore = true;
   };
 
-  let showModal = false;
+  $newPipelineVisibleStore = false;
 
 </script>
 
@@ -65,7 +64,7 @@
           </button>
           
           <button type="button" class="btn btn-secondary" id="btn-files"
-                  on:click={() => showModal = true}>
+                  on:click={() => newPipelineVisibleStore = true}>
             <i class="fas fa-file"></i> Files
           </button>
 
@@ -78,23 +77,3 @@
     </div>
   </div>
 </nav>
-
-{#if showModal}
-  <Modal on:close="{() => showModal = false}">
-    <h2 slot="header">
-      modal
-      <small><em>adjective</em>  mod·al \ˈmō-dəl\</small>
-    </h2>
-
-    <ol class="definition-list">
-      <li>of or relating to modality in logic</li>
-      <li>containing provisions as to the mode of procedure or the manner of taking effect —used of a contract or legacy</li>
-      <li>of or relating to a musical mode</li>
-      <li>of or relating to structure as opposed to substance</li>
-      <li>of, relating to, or constituting a grammatical form or category characteristically indicating predication</li>
-      <li>of or relating to a statistical mode</li>
-    </ol>
-
-    <a href="https://www.merriam-webster.com/dictionary/modal">merriam-webster.com</a>
-  </Modal>
-{/if}

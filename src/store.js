d08ac3a98732cf15f46b9c3b9b75b1d2a29c1d1d
Nilaksh Das <nilakshdas@gatech.edu>
2019-10-16 11:51:31 -0700

    Remove dependency on detailViewResetTimeoutStore

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { writable } from 'svelte/store';
 
 export const detailViewItemStore = writable(null);
-export const detailViewResetTimeoutStore = writable(null);
 export const newPipelineVisibleStore = writable(false);
 
 export const tokenStore = writable('');

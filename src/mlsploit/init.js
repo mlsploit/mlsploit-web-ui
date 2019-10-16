230b1eb001bfac3d6545c3feab2ea756716473fa
Nilaksh Das <nilakshdas@gatech.edu>
2019-10-16 12:22:13 -0700

    Add stores for data items, initialize stores with API data on app mount

--- src/mlsploit/init.js
+++ src/mlsploit/init.js
@@ -1,6 +1,20 @@
 import API from '../rest.js';
-import { fileStore } from '../store.js';
+import {
+  moduleStore,
+  functionStore,
+  fileStore,
+  pipelineStore,
+  taskStore,
+  runStore,
+  jobStore
+} from '../store.js';
 
-export const initStores = () => {
-  
+export const initStores = async () => {
+  moduleStore.set(await API.getModules());
+  functionStore.set(await API.getFunctions());
+  fileStore.set(await API.getFiles());
+  pipelineStore.set(await API.getPipelines());
+  taskStore.set(await API.getTasks());
+  runStore.set(await API.getRuns());
+  jobStore.set(await API.getJobs());
 };
--- src/store.js
+++ src/store.js
@@ -4,4 +4,11 @@ export const detailViewItemStore = writable(null);
 export const newPipelineVisibleStore = writable(false);
 
 export const tokenStore = writable('');
+
+export const moduleStore = writable([]);
+export const functionStore = writable([]);
 export const fileStore = writable([]);
+export const pipelineStore = writable([]);
+export const taskStore = writable([]);
+export const runStore = writable([]);
+export const jobStore = writable([]);

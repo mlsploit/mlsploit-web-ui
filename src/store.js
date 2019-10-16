6d29e0a8bed843150a7c1bf4739929807dc55f5f
Nilaksh Das <nilakshdas@gatech.edu>
2019-10-16 11:50:32 -0700

    Remove FileManagerVisibleStore not used anymore

--- src/store.js
+++ src/store.js
@@ -1,9 +1,8 @@
 import { writable } from 'svelte/store';
 
-export const newPipelineVisibleStore = writable(false);
-export const FileManagerVisibleStore = writable(false);
 export const detailViewItemStore = writable(null);
 export const detailViewResetTimeoutStore = writable(null);
+export const newPipelineVisibleStore = writable(false);
 
 export const tokenStore = writable('');
 export const fileStore = writable([]);

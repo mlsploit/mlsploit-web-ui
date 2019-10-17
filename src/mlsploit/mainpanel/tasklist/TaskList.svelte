451993a4c4799d7116e7daf8d6954140d890ea6c
Nilaksh Das <nilakshdas@gatech.edu>
2019-10-16 19:56:35 -0700

    Remove dependency on store for fade edge

--- src/mlsploit/mainpanel/tasklist/TaskList.svelte
+++ src/mlsploit/mainpanel/tasklist/TaskList.svelte
@@ -1,23 +1,17 @@
 <script>
   import { afterUpdate } from 'svelte';
   import { onMount } from 'svelte';
-  import { taskListIDOffsetStore } from '../../../store.js';
   import Task from './Task.svelte';
   import ImageVis from './ImageVis.svelte'
 
   export let tasks = [];
   export let showDropzone = false;
 
+  let taskListWrapperComponent;
   let taskListComponent;
   
   let numTasks = tasks.length;
   const hasNewPipelineTasks = Boolean(showDropzone);
-
-  // Need to create unique ID to distinguish from other TaskList component
-  let curID = $taskListIDOffsetStore;
-  taskListIDOffsetStore.set(curID + 1);
-  let leftEdge;
-  let rightEdge;
 
   const handleDragEnter = e => { e.target.classList.add('over'); };
   const handleDragLeave = e => { e.target.classList.remove('over'); };
@@ -35,40 +29,44 @@
     return false;
   };
 
-  afterUpdate(() => {
-    let currentNumTasks = tasks.length;
-    if (numTasks !== currentNumTasks) {
-      const taskElements = jQuery(taskListComponent).children('.task');
-      const newTaskElement = taskElements[currentNumTasks - 1];
-      jQuery(newTaskElement).focus();
-      numTasks = currentNumTasks;
-    }
-  });
+  const handleScrollPosition = () => {
+    let jqTaskList = jQuery(taskListComponent);
+    let jqLeftEdge = jQuery(taskListWrapperComponent).children('.fade-edge.left');
+    let jqRightEdge = jQuery(taskListWrapperComponent).children('.fade-edge.right');
 
-  function updateScrollPosition(taskList, leftEdge, rightEdge) {
     // Variables to track the progress of current scrolling
-    let scrolled = jQuery(taskList).scrollLeft();
-    let innerWidth = jQuery(taskList).innerWidth();
-    let contentWidth = jQuery(taskList)[0].scrollWidth;
+    let scrolled = jqTaskList.scrollLeft();
+    let innerWidth = jqTaskList.innerWidth();
+    let contentWidth = jqTaskList[0].scrollWidth;
 
     // User scrolls to the left end
     if (scrolled == 0) {
-      jQuery(leftEdge).css('opacity');
-      jQuery(leftEdge).css('opacity', 0.0);
+      jqLeftEdge.css('opacity');
+      jqLeftEdge.css('opacity', 0.0);
     
     // User scrolls to the right end
     } else if (scrolled + innerWidth >= contentWidth) {
-      jQuery(rightEdge).css('opacity');
-      jQuery(rightEdge).css('opacity', 0.0);
+      jqRightEdge.css('opacity');
+      jqRightEdge.css('opacity', 0.0);
 
     // User is in the middle of scrolling
     } else {
-      jQuery(leftEdge).css('opacity');
-      jQuery(leftEdge).css('opacity', 1.0);
-      jQuery(rightEdge).css('opacity');
-      jQuery(rightEdge).css('opacity', 1.0);
+      jqLeftEdge.css('opacity');
+      jqLeftEdge.css('opacity', 1.0);
+      jqRightEdge.css('opacity');
+      jqRightEdge.css('opacity', 1.0);
     }
-  }
+  };
+
+  afterUpdate(() => {
+    let newNumTasks = tasks.length;
+    if (numTasks !== newNumTasks) {
+      const taskElements = jQuery(taskListComponent).children('.task');
+      const newTaskElement = taskElements[newNumTasks - 1];
+      jQuery(newTaskElement).focus();
+      numTasks = newNumTasks;
+    }
+  });
 
   // Create input and output rule for each task in the task list
   let taskInputOutput = [];
@@ -95,15 +93,8 @@
   })
 
   onMount(() => {
-    let curTaskList = jQuery(taskListComponent).children(`#task-list-${curID}`);
-    let leftEdge = jQuery(taskListComponent).children(`#left-edge-${curID}`);
-    let rightEdge = jQuery(taskListComponent).children(`#right-edge-${curID}`);
-
-    updateScrollPosition(curTaskList, leftEdge, rightEdge);
-
-    curTaskList.on('scroll', function() {
-      updateScrollPosition(curTaskList, leftEdge, rightEdge);
-    })
+    handleScrollPosition();
+    jQuery(taskListComponent).on('scroll', handleScrollPosition);
   })
 
 </script>
@@ -174,49 +165,37 @@
 
 </style>
 
-<div class="task-list-wrapper" bind:this={taskListComponent}>
-  <div class="fade-edge left" id={`left-edge-${curID}`}></div>
-  <div class="fade-edge right" id={`right-edge-${curID}`}></div>
+<div class="task-list-wrapper" bind:this={taskListWrapperComponent}>
+  <div class="fade-edge left"></div>
+  <div class="fade-edge right"></div>
 
-  <div class="task-list rounded" id={`task-list-${curID}`}>
-    {#if !showDropzone}
+  <div class="task-list rounded" bind:this={taskListComponent}>
 
-      {#each tasks as task, idx}
-
-        <!-- Add an input component if this task requires to show one -->
-        {#if taskInputOutput[idx].showInput}
-          {#if taskInputOutput[idx].inputType === 'image'}
-          <ImageVis imageURL={taskInputOutput[idx].inputSource} isInput/>
-          {/if}
-          <i class="fas fa-arrow-right arrow"></i>
+    {#each tasks as task, idx}
+      <!-- Add an input component if this task requires to show one -->
+      {#if !hasNewPipelineTasks && taskInputOutput[idx].showInput}
+        {#if taskInputOutput[idx].inputType === 'image'}
+        <ImageVis imageURL={taskInputOutput[idx].inputSource} isInput/>
         {/if}
+        <i class="fas fa-arrow-right arrow"></i>
+      {/if}
 
-        <Task task={task} isNewPipelineTask={hasNewPipelineTasks}/>
+      <Task task={task} isNewPipelineTask={hasNewPipelineTasks}/>
 
-        <!-- Add an output component if this task requires to show one -->
-        {#if taskInputOutput[idx].showOutput}
-          <i class="fas fa-arrow-right arrow"></i>
-          {#if taskInputOutput[idx].outputType === 'image'}
-          <ImageVis imageURL={taskInputOutput[idx].outputSource} />
-          {/if}
+      <!-- Add an output component if this task requires to show one -->
+      {#if !hasNewPipelineTasks && taskInputOutput[idx].showOutput}
+        <i class="fas fa-arrow-right arrow"></i>
+        {#if taskInputOutput[idx].outputType === 'image'}
+        <ImageVis imageURL={taskInputOutput[idx].outputSource} />
         {/if}
+      {/if}
 
-        {#if idx !== tasks.length - 1}
-          <i class="fas fa-arrow-right arrow"></i>
-        {/if}
-      {/each}
-    {/if}
+      {#if idx !== tasks.length - 1}
+        <i class="fas fa-arrow-right arrow"></i>
+      {/if}
+    {/each}
 
     {#if showDropzone}
-      {#each tasks as task, idx}
-        {#if idx !== 0}
-          <i class="fas fa-arrow-right arrow"></i>
-        {/if}
-        
-        <Task task={task} isNewPipelineTask={hasNewPipelineTasks}>
-        </Task>
-      {/each}
-
       {#if tasks.length > 0}
         <i class="fas fa-arrow-right arrow"></i>
       {/if}
--- src/store.js
+++ src/store.js
@@ -8,8 +8,6 @@ export const tokenStore = writable('');
 export const moduleStore = writable([]);
 export const functionStore = writable([]);
 export const fileStore = writable([]);
-
-export const taskListIDOffsetStore = writable(0);
 export const pipelineStore = writable([]);
 export const taskStore = writable([]);
 export const runStore = writable([]);

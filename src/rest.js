19805583c16a31e5978ebadf4a2958a2ec8de6b4
RSLi <robertsli@163.com>
2019-10-21 09:23:57 -0400

    Add API bindings for POST, DELETE, PATCH requests

--- src/rest.js
+++ src/rest.js
@@ -51,8 +51,20 @@ const commonGETRequestOptions = {
   method: 'GET'
 };
 
+const commonDELETERequestOptions = {
+  ...commonRequestOptions,
+  method: 'DELETE'
+};
+
 const createGETRequestOptionsWithAuth = () => ({
   ...commonGETRequestOptions,
+  headers: {
+    'Authorization': `Token ${API.TOKEN}`
+  }
+});
+
+const createDELETERequestOptionsWithAuth = () => ({
+  ...commonDELETERequestOptions,
   headers: {
     'Authorization': `Token ${API.TOKEN}`
   }
@@ -74,6 +86,17 @@ const createPOSTRequestOptionsForObjWithAuth = (obj) => {
   return requestOptionsForObj;
 };
 
+const createPATCHRequestOptionsForObjWithAuth = (obj) => ({
+  ...commonRequestOptions,
+  method: 'PATCH',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-CSRFToken': getCookie('csrftoken'),
+    'Authorization': `Token ${API.TOKEN}`
+  },
+  body: JSON.stringify(obj)
+});
+
 const parseResponse = (response) => {
 
   if (!response.ok) {
@@ -84,10 +107,7 @@ const parseResponse = (response) => {
   return response.json();
 };
 
-API.getResourceFromURLWithAuth = resourceURL => fetch(
-  resourceURL,
-  createGETRequestOptionsWithAuth()
-).then(parseResponse);
+// Auth requests
 
 API.signin = (username, password) => fetch(
   API.ENDPOINTS.SIGNIN, 
@@ -103,6 +123,13 @@ API.signup = (username, password1, password2) => fetch(
   })
 ).then(parseResponse);
 
+// Query requests
+
+API.getResourceFromURLWithAuth = resourceURL => fetch(
+  resourceURL,
+  createGETRequestOptionsWithAuth()
+).then(parseResponse);
+
 API.getModules = () => API.getResourceFromURLWithAuth(API.ENDPOINTS.MODULES);
 API.getFunctions = () => API.getResourceFromURLWithAuth(API.ENDPOINTS.FUNCTIONS);
 API.getCurrentUser = () => API.getResourceFromURLWithAuth(API.ENDPOINTS.USER);
@@ -111,6 +138,67 @@ API.getPipelines = () => API.getResourceFromURLWithAuth(API.ENDPOINTS.PIPELINES)
 API.getTasks = () => API.getResourceFromURLWithAuth(API.ENDPOINTS.TASKS);
 API.getRuns = () => API.getResourceFromURLWithAuth(API.ENDPOINTS.RUNS);
 API.getJobs = () => API.getResourceFromURLWithAuth(API.ENDPOINTS.JOBS);
+
+// Delete requests
+
+API.deleteResourceFromURLWithAuth = resourceURL => fetch(
+  resourceURL,
+  createDELETERequestOptionsWithAuth()
+).then(parseResponse);
+
+// Mutation requests
+
+API.addTaskToPipeline = (pipelineURL, funcURL, args) => fetch(API.ENDPOINTS.TASKS, createPOSTRequestOptionsForObjWithAuth({
+  pipeline: pipelineURL,
+  function: funcURL,
+  arguments: JSON.stringify(args)
+})).then(parseResponse);
+
+API.enablePipeline = (pipelineURL) => fetch(pipelineURL, createPOSTRequestOptionsForObjWithAuth({
+  enabled: true
+}));
+
+API.createPipeline = (myPipeline) => fetch(API.ENDPOINTS.PIPELINES, createPOSTRequestOptionsForObjWithAuth({
+  name: myPipeline.name
+})).then(parseResponse).then(async pipeline => {
+  const pipelineURL = pipeline.url;
+  for (const component of myPipeline.components) {
+    await API.addTaskToPipeline(pipelineURL, component.funcURL, component.arguments);
+  }
+  await API.enablePipeline(pipelineURL);
+  return pipeline;
+});
+
+API.runPipeline = (pipelineURL, targetFileURLs) => fetch(API.ENDPOINTS.RUNS, createPOSTRequestOptionsForObjWithAuth({
+  pipeline: pipelineURL,
+  files: targetFileURLs,
+})).then(parseResponse);
+
+API.editFileTags = (file, tags) => fetch(resourceURL, createPATCHRequestOptionsForObjWithAuth({
+  tags: JSON.stringify(Object.assign(file.tags, tags))
+})).then(parseResponse);
+
+API.uploadFile = (file) => {
+  const formData = new FormData();
+  formData.append('blob', file);
+  // this request is the only post request that sends form data but not JSON.
+  return fetch(API.ENDPOINTS.FILES, {
+      headers: {
+          'Authorization': `Token ${window.token}`,
+          'X-CSRFToken': getCookie('csrftoken')
+      },
+      method: 'POST',
+      body: formData,
+  });
+};
+
+API.uploadFiles = (listOfFiles) => {
+  const listOfPromises = [];
+  for (let i = 0; i < listOfFiles.length; i++) {
+    listOfPromises.push(API.uploadFile(listOfFiles[i]));
+  }
+  return Promise.all(listOfPromises);
+};
 
 window.API = API;
 export default API;

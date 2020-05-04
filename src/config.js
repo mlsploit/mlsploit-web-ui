6b968886b24fea5bbc4f65ef6f2c7d9b0f1c1b3a
Nilaksh Das <nilakshdas@gatech.edu>
2020-05-04 13:41:24 -0700

    Disable debug mode by default

--- src/config.js
+++ src/config.js
@@ -1,12 +1,9 @@
-// const REST_API_HOST = window.location.protocol + '//' + window.location.hostname;
-// const REST_API_PORT = 8000;
-
-const REST_API_HOST = 'http://localhost'
-const REST_API_PORT = '8000';
+const REST_API_HOST = window.location.protocol + '//' + window.location.hostname;
+const REST_API_PORT = null; // sometimes hosted on port 8000
 
 const REST_API_URL = REST_API_HOST + (REST_API_PORT ? ':' + REST_API_PORT : '');
 
 export default {
-  DEBUG: true,
+  DEBUG: false,
   REST_API_URL: REST_API_URL,
 };
